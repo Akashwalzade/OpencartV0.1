9270a44533cadadfb152bef45e2c8eba7994c98e
Akashwalzade <walzadeakash2000@gmail.com>
2025-07-14 17:42:42 +0530

    search tc added

---
+++ src/test/java/pageObjects/SearchPage.java
@@ -0,0 +1,38 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class SearchPage extends BasePage {
+
+    public SearchPage(WebDriver driver) {
+        super(driver);
+    }
+
+    // Add meaningful locators instead of "xpathvalue"
+    @FindBy(name = "search")
+    WebElement searchBox;
+
+    @FindBy(css = "button.btn.btn-default.btn-lg")
+    WebElement searchButton;
+
+    // Example for product result
+    public boolean isProductExist(String productName) {
+        try {
+            return driver.findElement(By.linkText(productName)).isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public void enterProductName(String productName) {
+        searchBox.clear();
+        searchBox.sendKeys(productName);
+    }
+
+    public void clickSearchButton() {
+        searchButton.click();
+    }
+}
--- src/test/java/testCases/TC004_Search.java
+++ src/test/java/testCases/TC004_Search.java
@@ -1,5 +1,38 @@
 package testCases;
 
-public class TC004_Search {
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pageObjects.HomePage;
+import pageObjects.SearchPage;
+import testBase.BaseClass;
+
+public class TC004_Search extends BaseClass {
+	
+	@Test(groups = {"Regression", "Master"})
+    public void verify_search() throws InterruptedException {
+        logger.info("***** Starting TC004_Search *****");
+		try {
+			 HomePage hp = new HomePage(driver);
+
+	            SearchPage sp = new SearchPage(driver);
+	            sp.enterProductName("MacBook");
+	            sp.clickSearchButton();
+	            
+	            boolean result = sp.isProductExist("MacBook");
+	            Assert.assertTrue(result, "Product not found in search results!");
+			
+		}catch(Exception e) {
+			logger.error("Search test failed due to exception: " + e.getMessage());
+            Assert.fail();
+			
+		}
+		Thread.sleep(3000);
+		
+        logger.info("***** Finished  TC004_Search *****");
+
+		
+	}
+	
 
 }
